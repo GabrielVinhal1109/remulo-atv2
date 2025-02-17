266899c042f4e86dab3121c898091e12caf82f16
GabrielVinhal1109 <chuveirolorenzetti70@gmail.com>
2025-02-17 12:10:20 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,13 +1,36 @@
-import HelloWorld from "./HelloWorld";
-import Contador from "./Contador";
-import Mensagem from "./Mensagem";
-function App(){
-  return (
+import {useState} from "react";
+import TaskForm from "./TaskForm";
+import Tasklist from "./Tasklist";
+
+
+function App() {  
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (text) => {
+    setTasks([...tasks, {text, completed: false }]);
+};
+
+
+  const toggleTask = (index) => {
+    const newTasks = [...tasks];
+    nevTasks[index].completed= newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+
+  return(
     <div>
-        <HelloWorld />
-        <Contador />
-        <Mensagem />
+      <h1> Bem vindo a lista de tarefas! </h1>
+      <TaskForm onAddTask={addTask} />
+      <Tasklist tasks={tasks} onToggleTask={toggleTask} onDeleteTask={deleteTask}/>
     </div>
   );
 }
+
 export default App;
